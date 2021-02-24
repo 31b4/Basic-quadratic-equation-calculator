670fb82e68d08fdfb9dae64038a09632bdf36628
31b4 <75566095+Bence314@users.noreply.github.com>
2021-02-24 11:43:14 +0100

    English language update.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace x_2_megoldó_képlet
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace x_2_megoldó_képlet {
+    class Program {
+        static void Main(string[] args) {
             Console.Write("ax^2: \t");
             int ax = Convert.ToInt32(Console.ReadLine());
             Console.Write("bx: \t");
@@ -19,17 +16,12 @@ namespace x_2_megoldó_képlet
             Console.Clear();
             Console.WriteLine("ax^2 + bx + c\n");
             Console.WriteLine(ax+"x^2 + "+bx+"x + "+c+"\n");
-            
             int xpm=0;
-            if (bx<0)
-            {
+            if (bx<0){
                 bx = bx * (-1);
-                //Console.WriteLine(bx);
             }
-            else if (bx>0)
-            {
+            else if (bx>0) {
                 bx = -(bx);
-                //Console.WriteLine(xpm);
             }
             Console.WriteLine(@"
        _________
@@ -44,34 +36,27 @@ _________________
             Console.Write("{0}^2-4*{1}*{2}",bx,ax,c);
             Console.WriteLine("\n"+@"_________________");
             Console.WriteLine("\t2*"+ax+"\n");
-
-            int negyzet=bx*bx, nevezo=2*ax;
-            double gyok_alatt = negyzet - (4*ax*c);
+            int square = bx *bx, denominator = 2*ax;
+            double under_root = square - (4*ax*c);
             Console.Write(@"
        ___");
             Console.Write(
 "\n-" + bx + " +-");
             Console.Write(@"\|");
-            Console.Write("{0}", gyok_alatt );
+            Console.Write("{0}", under_root);
             Console.WriteLine("\n" + @"__________");
-            Console.WriteLine("    " + nevezo + "\n");
+            Console.WriteLine("    " + denominator + "\n");
             int x1;
             int x2;
-            if (gyok_alatt<0)
-            {
+            if (under_root < 0) {
                 Console.WriteLine("\nNincs megoldás");
             }
-            else
-            {   
-                x1 = (bx +((int)Math.Sqrt(gyok_alatt)))/nevezo;
-                x2 =(bx - ((int)Math.Sqrt(gyok_alatt)))/nevezo;
+            else {   
+                x1 = (bx +((int)Math.Sqrt(under_root)))/ denominator;
+                x2 =(bx - ((int)Math.Sqrt(under_root)))/ denominator;
                 Console.WriteLine("\nx1: "+x1);
                 Console.WriteLine("x2: "+x2);
             }
-
-
-
-
             Console.ReadKey();
         }
     }
